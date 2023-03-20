07f85588b41feb5502896e3df35c5700305a5d81
SHARAD PATEL <sharad.patels03@gmail.com>
2023-03-20 19:11:51 +0530

    h

--- src/rediscache/cache.module.ts
+++ src/rediscache/cache.module.ts
@@ -1,47 +1,21 @@
-// import { Module,CacheModule, CacheInterceptor} from "@nestjs/common";
-// import { APP_INTERCEPTOR } from "@nestjs/core";
-// import { cacheController } from "./cache.controller";
-// import { cacheService } from "./cache.service";
-// import * as redisStore from 'cache-manager-redis-store'
-
-
-// @Module({
-//     imports:[CacheModule.register({
-//         store:redisStore,
-//         socket: {host:'localhost',
-//         port:6379}
-//     })],
-//  controllers:[cacheController],
-// providers:[cacheService,{
-//     provide:APP_INTERCEPTOR,
-//     useClass:CacheInterceptor
-//  }],
-//  exports:[]
-// })
-// export class cacheModule{}
-import { Module, CacheModule, CacheInterceptor } from "@nestjs/common";
+import { Module,CacheModule, CacheInterceptor} from "@nestjs/common";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 import { CacheController } from "./cache.controller";
 import { CacheService } from "./cache.service";
-import redisStore from 'cache-manager-redis-store';
+import * as redisStore from 'cache-manager-redis-store'
+
+
 @Module({
-  imports: [
-    CacheModule.register({
-      store: redisStore,
-      config: {
-        host: 'localhost',
-        port: 6379,
-      },
-    }),
-  ],
-  controllers: [CacheController],
-  providers: [
-    CacheService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
-  exports: [CacheModule],
+    imports:[CacheModule.register({
+        store:redisStore,
+        socket: {host:'localhost',
+        port:6379}
+    })],
+ controllers:[CacheController],
+providers:[CacheService,{
+    provide:APP_INTERCEPTOR,
+    useClass:CacheInterceptor
+ }],
+ exports:[]
 })
-export class cacheModule {}
+export class cacheModule{}
